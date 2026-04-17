59762830c3097c21e3a61c3a8ad13623c49a0ce5
felipe120132-hash <felipe120132@gmail.com>
2026-04-17 13:17:35 -0300

    Corrigiendo importación de mysql y rutas

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const db = mysql.createConnection({
   host: "mysql-14c7894e-distriariza.d.aivencloud.com",
   port: 28140,
   user: "avnadmin",
-  password: "CONTRASEÑA_DE_AIVEN", // La que copiaste de la web
+  password: "AVNS_EbW_oAxV0nWCU-TNYKW", // La que copiaste de la web
   database: "defaultdb",
   ssl: {
     rejectUnauthorized: false // Necesario para conexiones externas
